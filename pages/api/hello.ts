a28254272aaddb3218ea845358708a416ffcdf22
areebaif <areeba.iftikhar1993@gmail.com>
2023-01-29 20:16:17 -0600

    Reimported prisma client

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,20 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+type Data = {
+  name: string;
+};
 
-export async function getStaticProps() {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
-  const data = await prisma.plot.findMany();
-  const plots = data;
-
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      plots,
-    },
-  };
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ name: "John Doe" });
 }
