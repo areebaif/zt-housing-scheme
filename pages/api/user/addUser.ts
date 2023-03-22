2fdbc65e852e7e780f486d6eade16a11c9bb4ea6
areebaif <areeba.iftikhar1993@gmail.com>
2023-03-22 16:05:27 -0500

    create user endpoint added

---
+++ pages/api/user/addUser.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../../db/prisma";
+import { TableRowItem } from "@/components/PlotIdPage/AddPaymentForm/PaymentInputTable";
+
+export interface PostReturnType {
+  created?: true;
+  error?: string;
+}
+
+export default async function addPayment(
+  req: NextApiRequest,
+  res: NextApiResponse<PostReturnType>
+) {
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const verifiedUser = req.body.verifiedUser;
+    const createUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        verifiedUser,
+      },
+    });
+    res.status(201).json({ created: true });
+  } catch (err) {
+    return res
+      .status(404)
+      .json({ error: "something went wrong please try again" });
+  }
+}
